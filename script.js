aaf70ec2599d03ff884276238768f2072120d91e
Vedant Pahariya <vedant.pahariya@research.iiit.ac.in>
2024-01-21 23:41:56 +0530

    fix the overlapping picture text

--- script.js
+++ script.js
@@ -21,31 +21,51 @@ function toggleMenu() {
             alert("Invalid input. Please enter a number between 1 and 9.");
         }
     }
-    function closePopup() {
-document.getElementById("overlay").style.display = "none";
-}
+    */
+//     function closePopup() {
+// document.getElementById("overlay").style.display = "none";
+// }
 
-function submitForm() {
-// Add your logic to handle form submission here
-// For now, let's just close the popup
-var time = parseInt(document.getElementById("name").value);
+// function submitForm(machineCode) {
+//     // Get the washing time from the form
+//     var time = parseInt(document.getElementById("name").value);
 
-    console.log("Number:", time);
-closePopup();
-}
-*/
+//     // Validate the input
+//     if (!isNaN(time) && time >= 1 && time <= 75) {
+//       console.log("Washing time for " + machineCode + ": " + time + " minutes");
+//       // Add your logic to handle the washing time here
+
+//       // Close the popup
+//       closePopup();
+//     } else {
+//       alert("Invalid input. Please enter a number between 1 and 75.");
+//     }
+//   }
+
+// function chooseTimer(machineName, machineCode) {
+//     // Prompt the user for input
+//     document.getElementById("overlay").style.display = "flex";
+    
+//     // Call submitForm with machineCode as an argument
+//     document.getElementById("inputForm").onsubmit = function() {
+//       submitForm(machineCode);
+//       return false; // prevent form submission
+//     };
+//   }
+
 
 function chooseTimer(machineName,machineCode){
     // Prompt the user for input
+    // document.getElementById("overlay").style.display = "flex";
     var timeInput = prompt("Enter washing time in minutes for " + machineName + ":");
 
     // Validate the input and perform necessary actions
-    if (timeInput !== null && !isNaN(timeInput) && timeInput >= 1 && timeInput <= 9) {
+    if (timeInput !== null && !isNaN(timeInput) && timeInput >= 1 && timeInput <= 75) {
         alert("Washing time set to " + timeInput + " minutes for " + machineName + ".");
         document.getElementById(machineCode).classList.toggle('close');
         // Add your logic to handle the washing time here
     } else {
-        alert("Invalid input. Please enter a number between 1 and 9.");
+        alert("Invalid input. Please enter a number between 1 and 75.");
     }
 }
 
