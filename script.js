472f6f341d23a17e74ac07851efc5398bfb2e7b1
Aryanil Panja <aryanilpanja@pop-os.localdomain>
2024-01-21 15:28:59 +0530

    made a js script file.

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("open");
+    var menuIcon = document.querySelector(".menu-icon");
+    menuIcon.classList.toggle("open");
+    }
